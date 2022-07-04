0d19088c7a7da0c81756e9f7bcd69ee3fcf9396b
Emmanuel0722 <107095260+Emmanuel0722@users.noreply.github.com>
2022-07-04 12:52:26 -0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MVCProject.Models;
+
+namespace MVCProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){}
+        public DbSet<Person>? Persons { get; set; }
+    }
+}
